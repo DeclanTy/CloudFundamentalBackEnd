a64d73de6a0dce68ad4fab22528db00dde76728e
declan-tyson <declan.j.tyson@gmail.com>
2021-12-14 14:22:39 +0000

    added @CrossOrigin

--- JavaProgramBackend/src/main/java/com/bae/JavaProgramBackend/property/web/propertyController.java
+++ JavaProgramBackend/src/main/java/com/bae/JavaProgramBackend/property/web/propertyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bae.JavaProgramBackend.property.domain.Property;
 import com.bae.JavaProgramBackend.property.service.PropertyServiceDB;
-
+@CrossOrigin
 @RestController
 public class propertyController {
 	private PropertyServiceDB service;
